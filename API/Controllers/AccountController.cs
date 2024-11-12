66c6c812b1ff444322311664f062e65588ed6328
bisan8 <bisan.laish@gmail.com>
2024-11-12 14:37:27 -0500

    End of section 8

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -15,23 +15,24 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Register(RegisterDto dto)
         {
             if (await UserExists(dto.Username)) return BadRequest("Username is taken!");
+            return Ok();
 
-            using var hmac = new HMACSHA512();
+            // using var hmac = new HMACSHA512();
 
-            var user = new AppUser
-            {
-                UserName = dto.Username.ToLower(),
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(dto.Password)),
-                PasswordSalt = hmac.Key
-            };
+            // var user = new AppUser
+            // {
+            //     UserName = dto.Username.ToLower(),
+            //     PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(dto.Password)),
+            //     PasswordSalt = hmac.Key
+            // };
 
-            context.Users.Add(user);
-            await context.SaveChangesAsync();
+            // context.Users.Add(user);
+            // await context.SaveChangesAsync();
 
-            return new UserDto {
-                Username = user.UserName,
-                Token = tokenService.CreateToken(user)
-            };
+            // return new UserDto {
+            //     Username = user.UserName,
+            //     Token = tokenService.CreateToken(user)
+            // };
         }
 
         [HttpPost("login")] // api/account/login
--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -4,7 +4,7 @@ namespace API.DTOs;
 
 public class RegisterDto
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
 
 }
